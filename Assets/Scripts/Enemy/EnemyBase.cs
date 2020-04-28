90eae11432c6fdcbd124edd3341f34448a7e2c14
dawninthemoon <clclekfl12@gmail.com>
2020-04-28 20:57:10 +0900

    폴더 정리

---
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBase : MonoBehaviour
+{
+    [SerializeField] protected float _moveSpeed;    
+    protected int _hp;
+}
---
+++ Assets/Scripts/Enemy/Enemy_Dummy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MonsterLove.StateMachine;
+
+public class Enemy_Dummy : EnemyBase
+{
+    public enum State { Idle, Move, Attack, Hurt, Die }
+
+    private StateMachine<State> _fsm;
+
+    private void Start() {
+        Initalize();
+    }
+
+    private void Initalize() {
+
+    }
+}

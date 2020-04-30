ef8716afaeaa1e381aef65cfc4f88ad7414abcbd
dawninthemoon <clclekfl12@gmail.com>
2020-04-30 22:23:37 +0900

    움직이면서 공격

---
+++ Assets/Scripts/StateMachineBehaviour/Player/DefaultMoveAttack1State.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultMoveAttack1State : AttackStateBase
+{
+    private static readonly int ClipCount = 7;
+
+    override protected void OnStateEntered(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {   
+        base.OnStateEntered(animator, stateInfo, layerIndex);
+        Context.EnterAttackProgress();
+        Context.SpawnAttackEffect(1);
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        RequestEnableHitbox(1, ClipCount);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Context.CurrentAttackState <= 1) {
+            Context.AttackEnd();
+        }
+    }
+}
